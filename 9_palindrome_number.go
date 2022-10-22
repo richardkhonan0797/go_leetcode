51a4a6ebfca1beee3bec429e8609cf33e369d31c
Richard Khonan <55327505+richardkhonan0797@users.noreply.github.com>
2022-10-22 16:09:57 +0700

    Update 9_palindrome_number.go

--- 9_palindrome_number.go
+++ 9_palindrome_number.go
@@ -5,28 +5,23 @@ import (
     "math"
 )
 
+func isPalindrome(x int) bool {
+    temp := x
+    rev := 0
+    
+    for temp > 0 {
+        rev = (rev * 10) + (temp % 10)
+        temp /= 10
+    }
+    
+    if rev == x {
+        return true
+    } else {
+        return false
+    }
+}
+
 func main() {
     fmt.Println(isPalindrome(121))
     fmt.Println(isPalindrome(321))
-}
-
-func isPalindrome(x int) bool {
-    reversed := 0
-    original := x
-    
-    for x > 0 {
-        right := x % 10
-        
-        power := math.Floor(math.Log10(float64(x)))
-        
-        reversed += right * int(math.Pow(10.0, power))
-        
-        x = int(math.Floor(float64(x)/10.0))
-    }
-    
-    if reversed == original {
-        return true
-    }
-    
-    return false
 }
